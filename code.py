6b89c3f3df2938263881c4f1f9e82e678facf1af
bhargav <bhargavkotte777@gmail.com>
2025-05-28 14:59:27 +0530

    ghjk

---
+++ code.py
@@ -0,0 +1,10 @@
+# for x in range(1,9):
+#     print(" ".join([str(x)]*x))
+
+# ['1']*1
+
+# ['2']*2 => ['2','2']
+# for x in range(1,9):
+#     print(f"{(str(x) + ' ') *x}")
+    # print((str(x)+' ')*x)
+    
